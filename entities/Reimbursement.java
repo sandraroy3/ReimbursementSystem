2ac4beaa2364bbd38266f909b4b5a62ebb2cddd9
sandraroy3 <62570600+sandraroy3@users.noreply.github.com>
2020-04-17 17:09:45 -0400

    Added Entity classes

---
+++ entities/Reimbursement.java
@@ -0,0 +1,70 @@
+package dev.sandra.entities;
+
+public class Reimbursement {
+
+	private int rId;
+	
+	private int amount;
+	private String description;
+	private String status;
+	
+	private int requesterId;
+	
+	
+	
+	public Reimbursement() {
+		super();
+	}
+	
+	
+	
+	public Reimbursement(int rId, int amount, String description, int requesterId, String status) {
+		super();
+		this.rId = rId;
+		this.amount = amount;
+		this.description = description;
+		this.requesterId = requesterId;
+		this.status = status;
+	}
+
+
+
+	public int getrId() {
+		return rId;
+	}
+	public void setrId(int rId) {
+		this.rId = rId;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getRequesterId() {
+		return requesterId;
+	}
+	public void setRequesterId(int requesterId) {
+		this.requesterId = requesterId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Reimbursement [rId=" + rId + ", amount=" + amount + ", description=" + description + ", requesterId="
+				+ requesterId + ", status=" + status + "]";
+	}
+	
+	
+}
